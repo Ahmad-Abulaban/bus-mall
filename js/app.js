6bc69df71fd69bebe093bd7dc562b94d59a59e54
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-08-16 15:11:12 +0300

    solved lab 12

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ let busImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfas
 let maxAttempts = 25;
 let attempt = 1;
 let products = [];
+let PNames = [];
+let votes = [];
+let times = [];
 
 function ProductImage(productName) {
     this.PName = productName.split('.')[0];
@@ -17,6 +20,7 @@ function ProductImage(productName) {
     this.votes = 0;
     this.times = 0;
     products.push(this);
+    PNames.push(this.PName);
 }
 
 for (let i = 0; i < busImages.length; i++) {
@@ -30,17 +34,25 @@ function randomImage() {
 let leftIndex;
 let CenterIndex;
 let rightIndex;
+let Tleft = -1;
+let Tcenter = -1;
+let Tright = -1;
 function renderImg() {
     leftIndex = randomImage();
     CenterIndex = randomImage();
     rightIndex = randomImage();
-    if (leftIndex === rightIndex) {
+    while (leftIndex === Tleft || leftIndex === Tcenter || leftIndex === Tright) {
         leftIndex = randomImage();
-    }else if(leftIndex === CenterIndex){
-        leftIndex = randomImage();
-    }else if(rightIndex === CenterIndex){
+    }
+    while(CenterIndex === leftIndex || CenterIndex === Tleft || CenterIndex === Tcenter || CenterIndex === Tright){
+        CenterIndex = randomImage();
+    }
+    while(rightIndex === CenterIndex || rightIndex === leftIndex || rightIndex === Tleft || rightIndex === Tcenter || rightIndex === Tright){
         rightIndex = randomImage();
     }
+    Tleft = leftIndex;
+    Tcenter = CenterIndex;
+    Tright = rightIndex;
     leftImg.setAttribute('src', products[leftIndex].PImg);
     CenterImg.setAttribute('src', products[CenterIndex].PImg);
     rightImg.setAttribute('src', products[rightIndex].PImg);
@@ -72,6 +84,7 @@ function clickHandler(event) {
         leftImg.removeEventListener('click', clickHandler);
         CenterImg.removeEventListener('click', clickHandler);
         rightImg.removeEventListener('click', clickHandler);
+
     }
 }
 
@@ -82,7 +95,50 @@ function final(){
             let liEl = document.createElement('li');
             result.appendChild(liEl);
             liEl.textContent = `${products[i].PName} had ${products[i].votes} votes and was seen ${products[i].times} times.`;
+            votes.push(products[i].votes);
+            times.push(products[i].times);
         }
         btn.removeEventListener('click', final);
+
     }
+    chartRender();
+}
+
+function chartRender(){
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: PNames,
+            datasets: [{
+                label: '# of votes',
+                data: votes,
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of times',
+                data: times,
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
